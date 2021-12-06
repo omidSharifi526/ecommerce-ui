d7c1cb47d2af1abc673d9aaf064de352a1434e07
DavoodSharifi <sharifidavood743@gmail.com>
2021-12-06 06:12:10 -0800

    create-project

---
+++ src/Components/InfoBar/InfoBar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './infobar-style.scss'
+
+const InfoBar = () => {
+    return (
+        <div className='container-fluid'>
+            <div className="infobar-top">
+                <div className='sale-ebay'>
+                    sample text
+
+                </div>
+                <div className='phone-number' >
+                    09566655222
+
+                </div>
+                <div className='send'>
+                    send to all
+
+                </div>
+
+            </div>
+            <div className="infobar-bottom">
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default InfoBar
